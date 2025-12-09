419a03cf0c1e02ae5d6ed45a62a820ed9dd50394
Thiagoolivs <thiago.olivs.coelho@gmail.com>
2025-12-09 02:54:06 -0300

    3º tentativa na correção de erros do crash-deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# GARANTE que o Django está no diretório certo
+cd /app  # <-- AJUSTAR para o caminho correto onde está manage.py
+
 echo "▶ Aguardando PostgreSQL..."
 
 while ! nc -z $PGHOST $PGPORT; do
@@ -10,7 +13,7 @@ done
 echo "✔ Banco disponível!"
 
 echo "▶ Migrações..."
-python manage.py migrate
+python manage.py migrate --noinput
 
 echo "▶ Static..."
 python manage.py collectstatic --noinput
@@ -22,4 +25,3 @@ python manage.py seed_skills || true
 
 echo "✔ Iniciando Gunicorn..."
 gunicorn talentmatch.wsgi:application --bind 0.0.0.0:8000
-
